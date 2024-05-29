328d3cc284a47f75639667b11aabd477fb3ec5fa
domandlar <domagoj.andlar@gmail.com>
2024-05-30 01:00:49 +0200

    Dodano kretanje igrača prema mišu.

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,39 +1,56 @@
 using Godot;
-using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+    public const float Speed = 300.0f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		//if (!IsOnFloor())
-			//velocity.Y += gravity * (float)delta;
+        // Get the input direction and handle the movement/deceleration.
+        Vector2 direction = Input.GetVector("left", "right", "up", "down");
+        if (direction != Vector2.Zero)
+        {
+            direction = GetDirection();
+            velocity.X = direction.X * Speed;
+            velocity.Y = direction.Y * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+        }
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+        Velocity = velocity;
+        MoveAndSlide();
+    }
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+    private Vector2 GetDirection()
+    {
+        Vector2 mousePosition = GetGlobalMousePosition();
+        Vector2 playerPosition = Position;
+        Vector2 direction = default;
+        Vector2 toMouse = (mousePosition - playerPosition).Normalized();
+        if (Input.IsActionPressed("up"))
+        {
+            direction = toMouse;
+        }
+        else if (Input.IsActionPressed("down"))
+        {
+            direction = -toMouse;
+        }
+        else if (Input.IsActionPressed("left"))
+        {
+            direction = toMouse.Rotated(-Mathf.Pi / 2);
+        }
+        else if (Input.IsActionPressed("right"))
+        {
+            direction = toMouse.Rotated(Mathf.Pi / 2);
+        }
+        return direction;
+    }
 }
